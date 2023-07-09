f9b005c4326ed2a7e00f67ef9e77859801e87194
emmajane1313 <admin@digitalax.xyz>
2023-07-08 20:13:01 -0400

    more set condition styling

--- src/components/CircuitFlow/modules/SetConditions/modules/ConditionInput/ContractCondition.tsx
+++ src/components/CircuitFlow/modules/SetConditions/modules/ConditionInput/ContractCondition.tsx
@@ -1,8 +1,12 @@
 import { FunctionComponent } from "react";
 import ConditionInput from "./ConditionInput";
 import Connector from "../../../Common/Connector";
+import { ContractConditionProps } from "@/components/CircuitFlow/types/circuitflow.types";
 
-const ContractCondition: FunctionComponent = (): JSX.Element => {
+const ContractCondition: FunctionComponent<ContractConditionProps> = ({
+  newContractConditionInformation,
+  setNewContractConditionInformation,
+}): JSX.Element => {
   return (
     <div className="relative flex flex-row items-center justify-center w-fit h-fit">
       <div
@@ -10,7 +14,36 @@ const ContractCondition: FunctionComponent = (): JSX.Element => {
         id="inputBorder"
       >
         <ConditionInput text="Enter contract address, ABI, event name, args and chain information." />
-        <div className="flex flex-col w-full h-full border-4 border-moda"></div>
+        <div className="flex w-full h-full border-4 border-moda overflow-y-scroll">
+          <div className="relative w-full h-fit gap-2 flex flex-col px-1.5 py-2.5">
+            <div className="relative w-full h-fit gap-1 flex flex-col">
+              <div
+                className="relative w-fit h-fit justify-start items-start flex font-vcr text-white text-sm"
+                id="blur"
+              >
+                Contract Address
+              </div>
+              <input
+                defaultValue={newContractConditionInformation?.contractAddress}
+                placeholder="enter contract address"
+                className="bg-aBlack w-full h-10 p-1 text-white font-vcr text-sm justify-start items-start flex"
+                id="borderLight"
+                onChange={(e) =>
+                  setNewContractConditionInformation({
+                    ...newContractConditionInformation!,
+                    contractAddress: e.target.value as any,
+                  })
+                }
+              />
+            </div>
+
+            <input
+              placeholder="contract address"
+              className="bg-aBlack w-full h-10 p-1 text-white font-vcr"
+              id="borderLight"
+            />
+          </div>
+        </div>
       </div>
       <Connector topOnly />
       <div
--- src/components/CircuitFlow/modules/SetConditions/modules/MoreConditionButton.tsx
+++ src/components/CircuitFlow/modules/SetConditions/modules/MoreConditionButton.tsx
@@ -3,25 +3,13 @@ import { setCircuitInformation } from "../../../../../../redux/reducers/circuitI
 import { MoreConditionButtonProps } from "@/components/CircuitFlow/types/circuitflow.types";
 
 const MoreConditionButton: FunctionComponent<MoreConditionButtonProps> = ({
-  dispatch,
-  circuitInformation,
-  newConditionInformation,
+  handleAddConditionAndReset,
 }): JSX.Element => {
   return (
     <div
       className="relative w-fit h-fit px-2.5 py-1.5 flex items-center justify-center cursor-pointer hover:opacity-50 active:scale-95"
       id="borderLight"
-      onClick={() =>
-        dispatch(
-          setCircuitInformation({
-            ...circuitInformation!,
-            conditions: [
-              ...circuitInformation!.conditions,
-              ...(newConditionInformation ? [newConditionInformation] : []),
-            ],
-          })
-        )
-      }
+      onClick={() => handleAddConditionAndReset()}
     >
       <div
         className="relative w-fit h-fit flex items-center justify-center font-vcr uppercase text-moda text-lg"
